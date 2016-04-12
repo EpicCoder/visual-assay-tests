cae0caf75a8026b39c9fa4f411c6cb2ca36b3e1b
EpicCoder <epinephrineds@gmail.com>
2016-04-13 02:18:49 +0600

    Setup base appium structure

---
+++ Visual Assay/android-test/Appium.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+
+namespace android_test
+{
+    public class Appium
+    {
+        private static Appium _instance;
+
+        public AndroidDriver<AndroidElement> Driver;
+
+        public static Appium Instance
+        {
+            get
+            {
+                if (_instance != null)
+                {
+                    return _instance;
+                }
+                return _instance = new Appium();
+            }
+        }
+
+        private Appium()
+        {            
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.SetCapability("app", Settings.Instance.AppPath);
+            capabilities.SetCapability("deviceName", "Google Nexus");
+            capabilities.SetCapability("platformName", "Android");
+            capabilities.SetCapability("autoLaunch", "false");
+            Driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), capabilities);
+        }
+    }
+}
---
+++ Visual Assay/android-test/Settings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace android_test
+{
+    class Settings
+    {
+        private static Settings _instance;
+
+        public string AppPath { get; private set; }
+
+        public static Settings Instance
+        {
+            get
+            {
+                if (_instance != null)
+                {
+                    return _instance;
+                }
+                return _instance = new Settings();
+            }
+        }
+
+        private Settings()
+        {
+            AppPath = ConfigurationManager.AppSettings["appPath"];
+        }
+    }
+}
