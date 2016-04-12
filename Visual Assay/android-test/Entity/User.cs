86d1db04438ad0237dcaa0461027c1605f7cc16a
EpicCoder <epinephrineds@gmail.com>
2016-04-13 02:29:26 +0600

    add user model

---
+++ Visual Assay/android-test/Entity/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace android_test.Entity
+{
+    class User
+    {
+        public string Name { get; private set; }
+        public string Password { get; private set; }
+
+        public User(string userName, string userPass)
+        {
+            Name = userName;
+            Password = userPass;
+        }
+    }
+}
