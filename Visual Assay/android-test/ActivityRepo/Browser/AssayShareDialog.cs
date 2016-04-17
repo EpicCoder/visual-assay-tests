2571713876aeeacad767e76feb814d2069b21646
EpicCoder <epinephrineds@gmail.com>
2016-04-17 07:32:37 +0600

    add share assay with user test

---
+++ Visual Assay/android-test/ActivityRepo/Browser/AssayShareDialog.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using android_test.ActivityElement;
+using android_test.Entity;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.MultiTouch;
+
+namespace android_test.ActivityRepo.Browser
+{
+    class AssayShareDialog
+    {
+        static string ActivityName = "Assay Share Dialog";
+
+        public static AndroidList TeamList
+        {
+            get
+            {
+                string id = "teams";
+                string name = "Team List";
+                return new AndroidList(id, name, ActivityName);
+            }
+        }
+
+        public static AndroidList TeamMemberList
+        {
+            get
+            {
+                string id = "users";
+                string name = "Team Member List";
+                return new AndroidList(id, name, ActivityName);
+            }
+        }
+
+        public static AndroidList ShareWithList
+        {
+            get
+            {
+                string id = "shared_with";
+                string name = "Share with List";
+                return new AndroidList(id, name, ActivityName);
+            }
+        }
+
+        public static AndroidLabel DropHere
+        {
+            get
+            {
+                string id = "drag_here_text";
+                string name = "Drop Here";
+                return new AndroidLabel(id, name, ActivityName);
+            }
+        }
+
+        public static AndroidButton Cancel
+        {
+            get
+            {
+                string id = "button_cancel";
+                string name = "Cancel";
+                return new AndroidButton(id, name, ActivityName);
+            }
+        }
+
+        public static AndroidButton Ok
+        {
+            get
+            {
+                string id = "button_share";
+                string name = "Ok";
+                return new AndroidButton(id, name, ActivityName);
+            }
+        }
+
+        public static void AddUser(string userName)
+        {
+            try
+            {
+                string selector =
+                    "new UiScrollable(new UiSelector().resourceId(\"com.assayrt:id/users\")).scrollIntoView(text(\"" +
+                    userName + "\"))";
+                AndroidElement dragItem =
+                    (AndroidElement)TeamMemberList.GetInternalElement().FindElementByAndroidUIAutomator(selector);
+                AndroidElement dropItem = DropHere.GetInternalElement();
+                TouchAction action = new TouchAction(Appium.Instance.Driver);
+                action.Press(dragItem).Wait(1500).MoveTo(dropItem).Release().Perform();
+                ConsoleMessage.Pass(String.Format("{0}. Drag user with name: {1} and drop to shareWith", ActivityName, userName));
+            }
+            catch (Exception ex)
+            {
+                ConsoleMessage.Fail(String.Format("{0}. Can't drag user with name: {1} and drop to shareWith", ActivityName, userName), ex);
+                throw;
+            }
+        }
+
+        public static void Unshare(string shareWith)
+        {
+            try
+            {
+                AndroidElement dragItem = (AndroidElement)ShareWithList.GetInternalElement().FindElementByName(shareWith);
+                AndroidElement dropItem = TeamList.GetInternalElement();
+                TouchAction action = new TouchAction(Appium.Instance.Driver);
+                action.Press(dragItem).Wait(1500).MoveTo(dragItem).Wait(1500).MoveTo(dropItem).Wait(1500).Release();
+                MultiAction multi = new MultiAction(Appium.Instance.Driver);
+                multi.Add(action).Perform();
+                ConsoleMessage.Pass(String.Format("{0}. Unshare. Drag user with name: {1} and drop to team", ActivityName, shareWith));
+            }
+            catch (Exception ex)
+            {
+                ConsoleMessage.Fail(String.Format("{0}. Can't unshare. Can't drag user with name: {1} and drop to team", ActivityName, shareWith), ex);
+                throw;
+            }
+        }
+
+        public static void SetPermission(Permission permission)
+        {
+            try
+            {
+                if (permission.View)
+                    Appium.Instance.Driver.FindElementById("chk_view").Click();
+                if (permission.Share)
+                    Appium.Instance.Driver.FindElementById("chk_share").Click();
+                if (permission.Modify)
+                    Appium.Instance.Driver.FindElementById("chk_modify").Click();
+                if (permission.Add)
+                    Appium.Instance.Driver.FindElementById("chk_add").Click();
+                ConsoleMessage.Pass(String.Format("{0}. Set permission", ActivityName));
+            }
+            catch (Exception ex)
+            {
+                ConsoleMessage.Fail(String.Format("{0}. Can't set permission", ActivityName), ex);
+                throw;
+            }
+        }
+
+        public static void ShareWithUserStep(string teamName, string userName, Permission permission)
+        {
+            TeamList.FindAndTap(teamName);
+            TeamMemberList.FindAndTap(userName);
+            AddUser(userName);
+            ShareWithList.VerifyElementCountById(1, "user_picture");
+            SetPermission(permission);
+            Ok.Tap();
+        }
+    }
+}
---
+++ Visual Assay/android-test/Test/UserScenario/ShareAssayWithUser.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using android_test.ActivityRepo;
+using android_test.ActivityRepo.Browser;
+using android_test.ActivityRepo.Flow;
+using android_test.ActivityRepo.Login;
+using android_test.Entity;
+using NUnit.Framework;
+
+namespace android_test.Test.UserScenario
+{
+    class ShareAssayWithUser
+    {
+        private User _user1;
+        private User _user2;
+        private string _team;
+        private int _timeout;
+        private int _shareDelay;
+        private int _loginDelay;
+        private string _version;
+        private string _assay;
+        private string _flow;
+
+        private string element1 = "Add";
+
+
+        [OneTimeSetUp]
+        public void BeforeClass()
+        {
+            _user1 = Settings.Instance.User1;
+            _user2 = Settings.Instance.User2;
+            _team = Settings.Instance.Team;
+            _timeout = Settings.Instance.LoginTimeout;
+            _shareDelay = Settings.Instance.ShareDelay;
+            _loginDelay = Settings.Instance.LoginDelay;
+            _version = Settings.Instance.Version;
+            _assay = string.Format("{0}-ShareAssayWithUser", _version);
+            _flow = string.Format("{0}-ShareAssayWithUser", _version);
+
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void ShareAsasyWithUserTest()
+        {
+            Permission permission = new Permission(true, true, true, true);
+
+            ConsoleMessage.StartTest("Share assay with user", "ShareAssay");
+            Appium.Instance.Driver.LaunchApp();
+            LoginActivity.LoginStep(_user1, _timeout);
+            BrowserActivity.CreateAssay(_assay);
+            BrowserActivity.CreateFlow(_flow);
+            BrowserActivity.FlowList.FindAndTap(_flow);
+            FlowActivity.AddElement(element1);
+            FlowActivity.ElementList.VerifyElementCountByClass(1, "android.widget.EditText");
+            TabMenu.Browser.Tap();
+            BrowserActivity.AssayList.FindAndTap(_assay);
+            BrowserActivity.ShareAssay.Tap();
+            AssayShareDialog.ShareWithUserStep(_team, _user2.Name, permission);
+            CommonOperation.Delay(_shareDelay);
+            TabMenu.Logout.Tap();
+
+            //verify shared assay
+            LoginActivity.LoginStep(_user2, _timeout);
+            CommonOperation.Delay(_loginDelay);
+            BrowserActivity.AssayList.FindAndTap("Shared With: " + _user1.Name);
+            BrowserActivity.FlowList.FindAndTap(_assay);
+            BrowserActivity.FlowList.FindAndTap(_flow);
+            FlowActivity.ElementList.VerifyElementCountByClass(1, "android.widget.EditText");
+            TabMenu.Logout.Tap();
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            Appium.Instance.Driver.CloseApp();
+            ConsoleMessage.EndTest();
+        }
+
+        [OneTimeTearDown]
+        public void AfterClass()
+        {
+            try
+            {
+                ConsoleMessage.StartTest("Share assay with user: Clean up", "ShareAssay");
+                Appium.Instance.Driver.LaunchApp();
+                LoginActivity.LoginStep(_user1, _timeout);
+                BrowserActivity.AssayList.FindAndTap(_assay);
+                BrowserActivity.DeleteAllFlows();
+                BrowserActivity.DeleteAssay.Tap();
+                AssayDeleteDialog.Delete.Tap();
+            }
+            finally
+            {
+                Appium.Instance.Driver.CloseApp();
+                ConsoleMessage.EndTest();
+            }
+        }
+    }
+}
